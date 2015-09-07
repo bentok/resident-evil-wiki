ea712331789db4e136b12af2ce5e39a26e81a366
Ben Copeland <bscopeland@gmail.com>
2015-09-06 22:38:44 -0400

    Getting API with JSONP and Angular

--- src/js/service.js
+++ src/js/service.js
@@ -5,9 +5,21 @@ angular
   .controller('ServiceController', ['$scope', '$http', serviceController]);
 
 function serviceController(scope, http) {
-  var url = 'js/re1.json';
-  http.get(url).success(function(response){
-      scope.data = response.results;
+
+  http({
+    method: 'JSONP',
+    url: 'http://www.giantbomb.com/api/game/3030-27715/' +
+         '?api_key=d2029bb22bd98ac08b54d148a4047617b22d3633' +
+         '&format=jsonp' +
+         '&json_callback=JSON_CALLBACK'
+  })
+  .success(function(data){
+      scope.data = data.results;
       console.log(scope.data);
-    });
+  });
+
+  // http.get(url).success(function(response){
+  //     scope.data = response.results;
+  //     console.log(scope.data);
+  //   });
 }
